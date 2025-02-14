7c3f4a9779afc3ea993c9a3390f97308441bef30
zunza <fksfks015@naver.com>
2025-02-14 16:48:11 +0900

    first commit

---
+++ src/main/java/zunza/tiketmon/waiting_server/WaitingServerApplication.java
@@ -0,0 +1,13 @@
+package zunza.tiketmon.waiting_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaitingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaitingServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/zunza/tiketmon/waiting_server/WaitingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package zunza.tiketmon.waiting_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaitingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
